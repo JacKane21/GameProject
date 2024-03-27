f3dc3b889f589f67a3f1f961236abf239a3c3b2b
JacKane21 <jk45111042723057004@proton.me>
2024-03-27 14:52:20 -0500

    Initial Commit (Window and GamePanel)

---
+++ src/main/Main.java
@@ -0,0 +1,24 @@
+package main;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Game Project");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+    }
+
+}
